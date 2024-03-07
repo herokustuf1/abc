bd7702af51de02b9f4475657f3331034bcf460bc
herokustuf1 <110223733+herokustuf1@users.noreply.github.com>
2024-03-07 21:42:41 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+ffmpeg -stream_loop -1 -re -i neffex.mp4 -stream_loop -1 -re -i http://stream.zeno.fm/9kaed9hws98uv -vcodec libx264 -pix_fmt yuvj420p -maxrate 500k -preset ultrafast -r 1 -g 50 -c:a aac -b:a 128k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv  rtmp://a.rtmp.youtube.com/live2/7re9-dmpd-atjf-x3k9-empv
